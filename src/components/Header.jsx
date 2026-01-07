69de59a44f112381ad1d9de1fe855fee57f515d3
Arthur Gehlen <1arthurgehlen@gmail.com>
2026-01-06 21:26:58 -0300

    Header finalizado

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import logo from "../images/logo.svg";
+import hamburguer_menu from "../images/bar.svg";
+
+const Header = () => {
+  return (
+    <header className="page_header">
+      <div className="header_main_area">
+        <img src={logo} alt="Logo" />
+        <ul className="header_links">
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+        </ul>
+      </div>
+      <div className="header_account_area">
+        <button className="login_btn">Login</button>
+        <button className="signup_btn">Sign Up</button>
+      </div>
+      <button className="mobile_hamburguer_menu">
+        <img src={hamburguer_menu} alt="Menu" />
+      </button>
+    </header>
+  );
+};
+
+export default Header;
